f75e6d41e0f9c11278009d3c78af31029855ee63
“nicolassistema” <nicolas.letticugna@gmail.com>
2020-02-12 01:30:03 -0300

    subiendo while del 1 al 4

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,7 +3,7 @@ function mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
-	//practicarlo bien
+	//practicarlo bien!!!!!
 	while(numero<1 || numero>9)
 	{
 		numero = prompt("ingrese un número entre 0 y 10.");
