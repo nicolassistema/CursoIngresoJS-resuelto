42ffa4942925e7594ee83ae52fe39e9bf510b8c4
“nicolassistema” <nicolas.letticugna@gmail.com>
2020-02-23 16:02:01 -0300

    subiendo ferretePinturerias con validaciones

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -4,10 +4,48 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+    var centigrados;
+    var faren;
+    var flag = 0;
+
+    faren = parseFloat(document.getElementById("Temperatura").value);
+
+    if (isNaN(faren)) {
+        alert("Por favor ingresar solamente numeros en elñ campo 'Ingrese Temperatura'");
+       flag = 1;
+        location.reload(true);
+    }
+
+    if (flag == 0) {
+    centigrados =  (5/9)*(faren-32);
+    centigrados = centigrados.toFixed(2); 
+
+    document.write(faren +" grados Fahrenheit son "+centigrados+" grados Centigrados ");
+    }
+
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    var centigrados;
+    var faren;
+    var flag = 0;
+  
+
+    centigrados = parseFloat(document.getElementById("Temperatura").value);
+    if (isNaN(centigrados)) {
+        alert("Por favor ingresar solamente numeros en elñ campo 'Ingrese Temperatura'");
+       flag = 1;
+        location.reload(true);
+    }
+
+    if (flag == 0) {
+    faren =  ((centigrados * 9)/5)+32;
+    faren = faren.toFixed(2); 
+    document.write(centigrados +" grados centigrados son "+faren+" grados Fahrenheit ");
+    }
+
+
+
+
 }
