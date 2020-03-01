870e56bea497b2cee3b11c3a82597edb00cf972a
“nicolassistema” <nicolas.letticugna@gmail.com>
2020-03-01 13:40:45 -0300

    subiendo fontecristo y algo del parcial

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,203 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
-{
- 	
+
+
+function NumerosPares() {
+//Variables    
+var numero;
+var numInicial;
+var numerosPar;
+var contador;
+
+//Entrada
+numero=parseInt(document.getElementById("numero").value);
+
+//Cargando valor inicial
+numInicial = numero;
+
+//Inicializador de conteo
+contador = 0;
+
+
+
+
+do {
+    //Calculador de numeros pares
+    numerosPar=numero%2;
+    /*
+    Flujo principal: 
+    1) contar si el numero tiene resto 0
+    2) Restarle 1 al numero ingresado para poder calcular nuevamente su resto
+    */
+    if (numerosPar == 0) {
+        contador ++;
+        numero = numero -1;
+    /*
+    Flujo alternativo:
+    1) Restarle 1 al numero ingresado para poder calcular nuevamente su resto
+    */
+    }else{
+        numero = numero -1;
+    }
+
+} while (numero != 0);
+
+alert("La cantidad de pares entre el numero ingresado "+numInicial+" y el 0 es: "+contador);
+
+}
+
+function NumerosImpares() {
+    
+    //Variables    
+var numero;
+var numInicial;
+var numerosImpar;
+var contador;
+
+//Entrada
+numero=parseInt(document.getElementById("numero").value);
+
+//Cargando valor inicial
+numInicial = numero;
+
+//Inicializador de conteo
+contador = 0;
+
+
+
+
+do {
+    //Calculador de numeros pares
+    numerosImpar=numero%2;
+    /*
+    Flujo principal: 
+    1) contar si el numero tiene resto != 0
+    2) Restarle 1 al numero ingresado para poder calcular nuevamente su resto
+    */
+    if (numerosImpar != 0) {
+        contador ++;
+        numero = numero -1;
+    /*
+    Flujo alternativo:
+    1) Restarle 1 al numero ingresado para poder calcular nuevamente su resto
+    */
+    }else{
+        numero = numero -1;
+    }
+
+} while (numero != 0);
+
+alert("La cantidad de impares entre el numero ingresado "+numInicial+" y el 0 es: "+contador);
+
+}
+
+
+function NumerosDivisibles(){
+
+//Variables    
+var numero;
+var numInicial;
+var numerosPar;
+var divisible;
+var contador;
+
+//Entrada
+numero=parseInt(document.getElementById("numero").value);
+
+//Cargando valor inicial
+numInicial = numero;
+
+//Inicializador de conteo
+contador = 0;
+divisible = 1;
+
+
+
+
+
+do {
+    //Calculador de numeros pares
+    numerosPar=divisible%numero;
+    console.log("Calculadore de pares: "+" | "+
+    "Resto: "+numerosPar+" | "+
+    "Numero divisible: "+divisible+" | "+
+    "Numero ingresado: "+numero);
+    /*
+    Flujo principal: 
+    1) contar si el numero tiene resto 0
+    2) Restarle 1 al numero ingresado para poder calcular nuevamente su resto
+    */
+    if (numerosPar == 0 ) {
+        contador ++;
+        console.log("contador: "+contador);
+        divisible = divisible + 1;
+        console.log("Divisible: "+divisible);
+    /*
+    Flujo alternativo:
+    1) Restarle 1 al numero ingresado para poder calcular nuevamente su resto
+    */
+    }else{
+        divisible = divisible +1;
+        console.log("Divisible: "+divisible);
+    }
+
+} while (divisible != 100);
+
+alert("La cantidad de numeros divisibles entre el 1 y el 100 para el numero ingresado "+numInicial+" es de: "+contador);
+
+
+
+
+
+}
+
+
+
+function VerificarPrimo() {
+  //var n1;
+  var primo;
+  var numero;
+  var contador;
+  contador = 1;
+  numero=parseInt(document.getElementById("numero").value);
+primo = 1; 
+  i=2;
+
+  while (i<numero) {
+      if (numero%i==0) {
+          primo=false;
+      }
+      i++;
+   
+  }
+
+if (primo==true) {
+    alert("es primo");
+}else{
+    alert("no es primo");
+}
+
+}
+
+function NumerosPrimos() {
+     //var n1;
+  var primo;
+  var numero;
+  var contador;
+  contador = 1;
+  numero=parseInt(document.getElementById("numero").value);
+primo = 1; 
+  i=2;
+
+  while (i<numero) {
+      if (numero%i==0) {
+          primo=false;
+      }
+      i++;
+      contador++;
+  }
+ alert("La cantidad de números Primos desde el número ingresado hasta el cero es de: "+contador); 
+    
+ 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,88 @@
 function mostrar()
 {
+var nota;
+var notaInc;
+var sexo;
+var i;
+var flag;
+var flag2;
+var contador;
+var minimo;
+var sexoUlt;
+var alumno;
+
+alumno = 1;
+flag2 = 0;
+flag = 0;
+i = 0;
+contador = 0;
+nota = 0;
+
+
+
+
+while (i < 3) {
+    sexo = prompt("Ingresar Sexo del alumno: "+alumno);
+    sexo = sexo.toLowerCase();
+    nota = parseInt(prompt("Ingresar nota del alumno: "+alumno));
+    console.log("ingresar nota: "+nota);
+    //nota = parent(nota);
+
+    //sumador de notas
+    
+    if (flag == 0) {
+        notaInc = nota;
+        flag =1;
+    }else{
+
+        notaInc = nota + notaInc;
+    }
+    
+
+//observamos si son maximos y minimos
+if (flag2 == 0) {
+    //Entrariamos la primera vez y solo una vez
+    minimo = nota;
+    minimo=parseInt(minimo);
+    sexoUlt = sexo;
+    console.log("if: "+minimo);
+    //console.log("if: "+sexoUlt);
+    flag2 = 1;
+}else if (nota < minimo) {
+    minimo = nota;
+    sexoUlt = sexo;
+
+    console.log("else: "+minimo);
+    console.log("else: "+sexoUlt);
+    
+}
+    
+
+//Cantidad de alumnos con nora > = a 6
+    if (sexo =="m" && nota >= 6) {
+        contador++;
+    }
+
+    i++;
+    alumno++;
+
+
+
+}
+
+
+promedio= notaInc/5;
+alert("La nota mas baja es: "+minimo+" "+sexoUlt+"\n"+
+"El promedio total de notas es de: "+promedio+"\n"+
+"La cantidad de varones con nota mayor o igual a 6 es de: "+contador
+);
+
+
+
+
+
+
+
+
 
 }
